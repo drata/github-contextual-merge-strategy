0e52425285e8ebd17770647acd018655816e8933
carmil <56884489+carmilx@users.noreply.github.com>
2024-08-21 09:28:56 -0700

    chore(update): update extension to look for rc branches

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
 /* global MutationObserver */
 
-function updateStrategy () {
+function updateStrategy() {
   const branch = document.querySelector('.head-ref').textContent;
-  if (branch.startsWith('release/')) {
-    // do a MERGE if the branch is a release branch
-    // console.log('ONLY merge')
-    document.querySelector('.merge-message details button[value=merge]').click()
-    // hide the other buttons
+
+  if (branch === 'release') {
+    // For PRs targeting the release branch, use Squash and Merge
+    document.querySelector('.merge-message details button[value=squash]').click();
+    // Hide the other buttons
+    document.querySelector('.merge-message details button[value=merge]').style.visibility = "hidden";
+    document.querySelector('.merge-message details button[value=rebase]').style.visibility = "hidden";
+
+  } else if (branch.startsWith('rc/')) {
+    // For PRs targeting main from rc/* branches, use Merge Commit
+    document.querySelector('.merge-message details button[value=merge]').click();
+    // Hide the other buttons
     document.querySelector('.merge-message details button[value=squash]').style.visibility = "hidden";
     document.querySelector('.merge-message details button[value=rebase]').style.visibility = "hidden";
-  } else {
-    // Otherwise squash by default
-    // console.log('ONLY squash and merge')
-    document.querySelector('.merge-message details button[value=squash]').click()
-    // hide the other buttons
+
+  } else if (branch === 'main') {
+    // For PRs targeting main from any other branches (including hotfix/*), use Squash and Merge
+    document.querySelector('.merge-message details button[value=squash]').click();
+    // Hide the other buttons
     document.querySelector('.merge-message details button[value=merge]').style.visibility = "hidden";
     document.querySelector('.merge-message details button[value=rebase]').style.visibility = "hidden";
   }
 }
 
-function main () {
-  const details = document.querySelector('.merge-message details')
+function main() {
+  const details = document.querySelector('.merge-message details');
 
   if (details) {
-    updateStrategy()
-    // Still watch for updates, needed especially for race conditions in single-page navigation
+    updateStrategy();
   }
 
-  const actions = document.querySelector('.discussion-timeline-actions')
+  const actions = document.querySelector('.discussion-timeline-actions');
 
   if (!actions) {
-    // Likely navigation fired before updating DOM but it typically fires again after
-    return
+    return;
   }
 
   const observer = new MutationObserver(() => {
     if (document.querySelector('.merge-message details')) {
-      observer.disconnect()
-      // console.log('Merge message updated, running')
-      updateStrategy()
-    } else {
-      // console.log('Merge message updated, not ready')
+      observer.disconnect();
+      updateStrategy();
     }
-  })
+  });
 
-  observer.observe(actions, { subtree: true, childList: true })
+  observer.observe(actions, { subtree: true, childList: true });
 }
 
-main()
+main();
